e342a940d16020508a32085072b45c0330b8636e
jacobege <jacob.ege@gmail.com>
2016-05-11 15:16:29 +0200

    cleanup

--- ConcurrentUserTest1/Utility.cs
+++ ConcurrentUserTest1/Utility.cs
@@ -5,11 +5,11 @@ namespace ConcurrentUserTest1
 {
     public class Utility
     {
-    private static string CONNECTION_STRING = string.Format("Server = {0}; Database = {1}; Uid = {2}; Pwd = {3};",
-                                                            "77.66.48.115",
-                                                            "cluster_b",
-                                                            "cluster_b",
-                                                            "password");
+        private static string CONNECTION_STRING = string.Format("Server = {0}; Database = {1}; Uid = {2}; Pwd = {3};",
+                                                                "77.66.48.115",
+                                                                "cluster_b",
+                                                                "cluster_b",
+                                                                "password");
         public static void HandleConnection(Action<MySqlConnection> method)
         {
             using (MySqlConnection conn = new MySqlConnection())
