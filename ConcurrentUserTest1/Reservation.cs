73fe60051d3ca52094db91737f9e404f63a4ed0e
jacobege <jacob.ege@gmail.com>
2016-05-11 15:29:16 +0200

    missing stuff

--- ConcurrentUserTest1/Reservation.cs
+++ ConcurrentUserTest1/Reservation.cs
@@ -39,8 +39,62 @@ namespace ConcurrentUserTest1
             return seatNo;
         }
 
+        public int book(string plane_no, string seat_no, long id)
+        {
+            //Make sure that the id has reserved the seat, and then attempt to book it
+            switch (id)
+            {
+                case 0:
+                    //Booked successfully
+                    return 0;
+                case -1:
+                    //Not reserved at all
+                    return -1;
+                case -2:
+                    //Not reserved by this customer id
+                    return -2;
+                case -3:
+                    //Reservation timeout
+                    return -3;
+                case -4:
+                    //Seat is already booked
+                    return -4;
+                case -5:
+                    //All other errors
+                    return -5;
+                default:
+                    return 1;
+            }
+        }
+
+        public void bookAll(string plane_no)
+        {
+
+        }
+
+        public bool isAllBooked(string plane_no)
+        {
+            var command = new MySqlCommand("SELECT plane_no FROM seat where plane_no = @plane_no and ;", conn);
+            var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                var seatNo = reader.GetString("seat_no");
+                Console.Out.WriteLine(seatNo);
+            }
+            return true;
+        }
+
         public bool isAllReserved(string plane_no)
         {
+            var command = new MySqlCommand("SELECT * FROM seat;", conn);
+            var reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                var seatNo = reader.GetString("seat_no");
+                Console.Out.WriteLine(seatNo);
+            }
             return true;
         }
     }
