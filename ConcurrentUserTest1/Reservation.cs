88b8970feed9961d839b7161c1d6a35ebfc937bd
Saphe <maanssonlund@gmail.com>
2016-05-11 14:47:23 +0200

    Reservation outline

--- ConcurrentUserTest1/Reservation.cs
+++ ConcurrentUserTest1/Reservation.cs
@@ -1,28 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConcurrentUserTest1
 {
-    class Reservation
+    public class Reservation
     {
+        string user;
+        string pw;
 
-
-        public Reservation(String User, String PW) {
-
+        public Reservation(string user, string pw)
+        {
+            this.user = user;
+            this.pw = pw;
         }
 
-        public String reserve(String planeNo, long ID)
+        public string reserve(string plane_no, long id)
         {
-            String seatNo = null;
-            return seatNo;
+            //Return free seat where plane_no = plane_no and reserved = null and booked = null
+
+            return "";
         }
 
-        public int book(String planeNo, String seatNo, long ID)
+        public int book(string plane_no, string seat_no, long id)
         {
-            return 0;
+            //Make sure that the id has reserved the seat, and then attempt to book it
+            switch (id)
+            {
+                case 0:
+                    //Booked successfully
+                    return 0;
+                case -1:
+                    //Not reserved at all
+                    return -1;
+                case -2:
+                    //Not reserved by this customer id
+                    return -2;
+                case -3:
+                    //Reservation timeout
+                    return -3;
+                case -4:
+                    //Seat is already booked
+                    return -4;
+                case -5:
+                    //All other errors
+                    return -5;
+                default:
+                    return 1;
+            }
+        }
+
+        public void bookAll(string plane_no)
+        {
+        }
+
+        public void clearAllBookings(string plane_no)
+        {
+        }
+
+        public bool isAllBooked(string plane_no)
+        {
+            return true;
+        }
+
+        public bool isAllReserved(string plane_no)
+        {
+            return true;
         }
     }
 }
