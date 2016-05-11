511a85a3c950cf68a067861dec8d811b585c8213
seansemerson <cph-se15@cphbusiness.dk>
2016-05-11 14:16:40 +0200

    Reservation class skelet

---
+++ ConcurrentUserTest1/Reservation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcurrentUserTest1
+{
+    class Reservation
+    {
+
+
+        public Reservation(String User, String PW) {
+
+        }
+
+        public String reserve(String planeNo, long ID)
+        {
+            String seatNo = null;
+            return seatNo;
+        }
+
+        public int book(String planeNo, String seatNo, long ID)
+        {
+            return 0;
+        }
+    }
+}
