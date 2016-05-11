26acd852e74d038600bbe67d32bff1ce5d71f146
jacobege <jacob.ege@gmail.com>
2016-05-11 14:47:58 +0200

    Renamed some stuff

---
+++ ConcurrentUserTest1/Utility.cs
@@ -0,0 +1,23 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+namespace ConcurrentUserTest1
+{
+    public class Utility
+    {
+        public static void HandleConnection(Action<MySqlConnection> method)
+        {
+            using (MySqlConnection conn = new MySqlConnection())
+            {
+                conn.ConnectionString = string.Format("Server = {0}; Database = {1}; Uid = {2}; Pwd = {3};",
+                                                            "77.66.48.115",
+                                                            "cluster_b",
+                                                            "cluster_b",
+                                                            "password");
+                conn.Open();
+                method(conn);
+            }
+
+        }
+    }
+}
